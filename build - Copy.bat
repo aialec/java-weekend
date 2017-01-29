76f991d04118d7ab21203cda56c8df48237b65e1
aialec <aialec90@gmail.com>
2017-01-29 12:19:01 +0100

    basic_auth

---
+++ build - Copy.bat
@@ -0,0 +1,2 @@
+del c:\workspace\xampp\tomcat\webapps\javaWeekend.war
+RMDIR c:\workspace\xampp\tomcat\webapps\javaWeekend /s /q
--- build.bat
+++ build.bat
@@ -1,9 +1,10 @@
 set JAVA_HOME=c:\Program Files\Java\jdk1.8.0_121
 call mvn clean install
-del c:\workspace\xampp\tomcat\webapps\javaWeekend /q
+del c:\workspace\xampp\tomcat\webapps\javaWeekend.war
+RMDIR c:\workspace\xampp\tomcat\webapps\javaWeekend /s /q
 copy target\javaWeekend.war c:\workspace\xampp\tomcat\webapps\
 cd c:\workspace\xampp\tomcat\bin\
-del c:\workspace\xampp\tomcat\logs\* /q
+del c:\workspace\xampp\tomcat\logs\* /s /q
 call shutdown.bat
 timeout 3
 call catalina.bat jpda start
